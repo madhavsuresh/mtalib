e1db3ca4db161739724361c042f355e9119f91cf
Nathan Shelly <nathanshelly2018@u.northwestern.edu>
2016-09-06 23:34:48 -0500

    Refactored methods to accept normal parameters instead of dicts, added docstrings for course endpoint methods

--- api.py
+++ api.py
@@ -1,39 +1,49 @@
 import requests, json, string
 
 class server_accessor:
+  """Class for accessing the Mechanical TA API"""
   server_url = ''
 
   def __init__(self, p_server_url):
+    """Creates a server_accessor instance which will make call to the specified url"""
     if p_server_url[-1:] != '/':
       p_server_url += '/'
     self.server_url = p_server_url;
 
   def __str__(self):
+    """Prints the url this instance is accessing"""
     return self.server_url
 
   def create_course(self, name, displayName, authType='pdo', registrationType='Open', browsable=True):
-    print self.server_url + 'course/create'
-    locals()['displayName'] = displayName
-    print locals()
-    locals()['name'] = name
-    print locals()
+    """Creates a course with optional values. name and displayName are required parameters and name must be unique for call to not return error"""
+    course_params = locals()
+    # hacky and ugly, not particularly robust, look to change in future
+    course_params.pop('self', None)
     r = requests.post(self.server_url + 'course/create', data=json.dumps(course_params))
     return r
 
-  def update_course(self, course_params):
+  def update_course(self, courseID, name='', displayName='', authType='', registrationType='', browsable=''):
+    """Updates course specified by courseID with any additional optional parameters specified by user"""
+    params = locals()
+    # hacky and ugly, not particularly robust, look to change in future
+    params.pop('self', None)
+    course_params = {key:value for (key, value) in params.iteritems() if value}
+
     r = requests.post(self.server_url + 'course/update', data=json.dumps(course_params))
     return r
 
   def delete_course(self, courseID):
+    """Deletes the course specified by ID"""
     delete_data = {'courseID' : courseID}
     r = requests.post(self.server_url + 'course/delete', data=json.dumps(delete_data))
     return r
 
   def get_course(self, courseID=""):
+    """If a courseID is supplied this returns the information associated with that class, without a specified courseID this returns a list of courses with courseID, name, displayName and browsable values for each course"""
     get_data = {}
     if courseID:
       get_data['courseID'] = courseID
-      r = requests.get(self.server_url + 'course/get', data=json.dumps(get_data))
+    r = requests.get(self.server_url + 'course/get', data=json.dumps(get_data))
     return r
 
 ################################## Assignments ###############################################3#
