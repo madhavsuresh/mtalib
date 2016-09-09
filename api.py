c67fb02949ea918b91254bbc768e8bafff2e5f64
Nathan Shelly <nathanshelly2018@u.northwestern.edu>
2016-09-09 07:38:41 +0100

    Refactored assignment, rubric and testing calls

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-import requests, json, string
+import requests, json, string, copy, pprint
 
 class server_accessor:
   """Class for accessing the Mechanical TA API"""
@@ -68,53 +68,68 @@ class server_accessor:
       get_data['users'] = list_of_users
     return requests.get(self.server_url + 'user/get', data = json.dumps(get_data))
 
+################################## Assignments ###############################################
 
-################################## Assignments ###############################################3#
+  def create_assignment(self, courseID, name, submissionQuestion, submissionStartDate = 1472352458, submissionStopDate = 2472352458, reviewStartDate = 1472352458, reviewStopDate = 2472352458, markPostDate = 2472352458, appealStopDate = 2472352458, maxSubmissionScore = 10, maxReviewScore = 5, defaultNumberOfReviews = 3, submissionType = 'essay'):
+    """Creates an assignment based on the passed in parameters and on hardcoded defaults"""
+    assignment_params = locals()
+    assignment_params.pop('self', None)
+    defaults = {'password' : None, 'passwordMessage' : None, 'visibleToStudents' : 1, 'assignmentType' : 'peerreview', 'dateFormat' : 'MMMM Do YYYY, HH:mm', 'calibrationPoolAssignmentIds' : [], 'extraCalibrations' : 0, 'calibrationStartDate' : 0, 'calibrationStopDate' : 0, 'showMarksForReviewsReceived' : 1, 'showOtherReviewsByStudents' : 0, 'showOtherReviewsByInstructors' : 0, 'showMarksForOtherReviews' : 0, 'showMarksForReviewedSubmissions' : 0, 'showPoolStatus' : 0, 'calibrationMinCount' : 0,'calibrationMaxScore' : 0,'calibrationThresholdMSE' : 0,'calibrationThresholdScore' : 0, 'allowRequestOfReviews' : 0, 'submissionSettings' : {'topics' : [], 'autoAssignEssayTopic' : 1, 'essayWordLimit' : 10000}}
 
-  def create_assignment(self, assignment_params):
-    print self.server_url + 'create/assignment'
-    r = requests.post(self.server_url + 'assignment/create', data = json.dumps(assignment_params))
-    print r
-    return r
+    defaults.update(assignment_params)
+    return requests.post(self.server_url + 'assignment/create', data = json.dumps(defaults))
 
-  def update_assignment(self, assignment_params):
-    print self.server_url + 'update/assignment'
-    r = requests.post(self.server_url + 'assignment/update', data = json.dumps(assignment_params))
-    return r
+  def update_assignment(self, courseID, name, submission_question, submission_start_date = 1472352458, submission_stop_date = 2472352458, review_start_date = 1472352458, review_stop_date = 2472352458, mark_post_date = 2472352458, appeal_stop_date = 2472352458, max_submission_score = 10, max_review_score = 5, default_number_of_reviews = 3, submission_type = 'essay'):
+    """Updates an assignment based on the passed in parameters and on hardcoded defaults"""
+    assignment_params = locals()
+    assignment_params.pop('self', None)
+    defaults = {'password' : None, 'passwordMessage' : None, 'visibleToStudents' : 1, 'assignmentType' : 'peerreview', 'dateFormat' : 'MMMM Do YYYY, HH:mm', 'calibrationPoolAssignmentIds' : [], 'extraCalibrations' : 0, 'calibrationStartDate' : 0, 'calibrationStopDate' : 0, 'showMarksForReviewsReceived' : 1, 'showOtherReviewsByStudents' : 0, 'showOtherReviewsByInstructors' : 0, 'showMarksForOtherReviews' : 0, 'showMarksForReviewedSubmissions' : 0, 'showPoolStatus' : 0, 'calibrationMinCount' : 0,'calibrationMaxScore' : 0,'calibrationThresholdMSE' : 0,'calibrationThresholdScore' : 0, 'allowRequestOfReviews' : 0, 'submissionSettings' : {'topics' : [], 'autoAssignEssayTopic' : 1, 'essayWordLimit' : 10000}}
 
-  def get_assignment(self, params):
-    print self.server_url + 'get/assignment'
-    r = requests.get(self.server_url + 'assignment/get', data = json.dumps(params))
-    return r
+    defaults.update(assignment_params)
+    return requests.post(self.server_url + 'assignment/update', data = json.dumps(assignment_params))
 
-##################################rubrics ##############################################
+  def get_assignment(self, courseID, assignmentIDs):
+    """Takes a courseID and a list of assignmentIDs and returns the information of the given assignments"""
+    assignment_params = locals()
+    assignment_params.pop('self', None)
+    print assignment_params
+    return requests.get(self.server_url + 'assignment/get', data = json.dumps(assignment_params))
 
-  def create_rubric(self, rubric_params):
-    print self.server_url + 'rubric/create'
-    r = requests.post(self.server_url + 'rubric/create', data = json.dumps(rubric_params))
-    return r
+################################## Rubrics ##########################################
 
-  def get_rubric(self, assignment_params):
+  def create_rubric(self, courseID, assignmentID, name, question = 'test question?', hidden = 0, displayPriority = 0, options = [{'label' : 'A' , 'score' : 5.0}, {'label' : 'B' , 'score' : 4.0}, {'label' : 'C' , 'score' : 3.0}, {'label' : 'D' , 'score' : 2.0}, {'label' : 'E' , 'score' : 1.0}]):
+    """Creates rubric for given courseID and assignmentID with given name"""
+    rubric_params = locals()
+    rubric_params.pop('self', None)
+
+    return requests.post(self.server_url + 'rubric/create', data = json.dumps(rubric_params))
+
+  def update_rubric(self, courseID, assignmentID, name, question = 'test question?', hidden = 0, displayPriority = 0, options = [{'label' : 'A' , 'score' : 5.0}, {'label' : 'B' , 'score' : 4.0}, {'label' : 'C' , 'score' : 3.0}, {'label' : 'D' , 'score' : 2.0}, {'label' : 'E' , 'score' : 1.0}]):
+    """Creates rubric for given courseID and assignmentID with given name"""
+    rubric_params = locals()
+    rubric_params.pop('self', None)
+
+    return requests.post(self.server_url + 'rubric/update', data = json.dumps(rubric_params))
+
+  def get_rubric(self, assignmentID):
     '''Gets all rubrics right now based on AssignmentID '''
-    #$print self.server_url + 'rubric/get'
-    print "world"
-    r = requests.get(self.server_url + 'rubric/get', data = json.dumps(assignment_params))
-    print "hello"
-    return r
-
-  def update_rubric(self, assignment_params):
-    print self.server_url + 'rubric/update'
-    r = requests.post(self.server_url + 'rubric/update', data = json.dumps(assignment_params))
-    return r
+    assignment_params = locals()
+    assignment_params.pop('self', None)
+    return requests.get(self.server_url + 'rubric/get', data = json.dumps(assignment_params))
 
 ############################### TESTING ##############################
 
-  def make_submissions(self, params):
-    print self.server_url + "makesubmissions"
-    r = requests.post(self.server_url + 'makesubmissions', data = json.dumps(params))
-    return r
+  def make_submissions(self, courseID, assignmentID):
+    make_submissions_params = locals()
+    make_submissions_params.pop('self', None)
+
+    return requests.post(self.server_url + 'makesubmissions', data = json.dumps(make_submissions_params))
 
   def create_peerreviewscores(self, peerreviewscores_params):
-    print self.server_url + "peerreviewscores/create"
-    r = requests.post(self.server_url + "peerreviewscores/create", data = json.dumps(peerreviewscores_params))
-    return r
+    return requests.post(self.server_url + 'peerreviewscores/create', data = json.dumps(peerreviewscores_params))
+
+  def get_peerreviewscores(self, ):
+    pass
+
+  def get_course_id_from_name(self, course_name):
+    return requests.get(self.server_url + 'getcourseidfromname', data = json.dumps({'courseName' : course_name}))
