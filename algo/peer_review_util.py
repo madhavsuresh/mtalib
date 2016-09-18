f1bd755a09c217cf70613d658aee0e8af077748e
Jason Hartline <jasonhartline@gmail.com>
2016-09-18 11:33:47 -0500

    added kkv (key,key,value) utilities

--- algo/peer_review_util.py
+++ algo/peer_review_util.py
@@ -11,3 +11,31 @@ def invert_dictlist_dup(d):
 #note this will only work on hashable objects
 def duplicates(tocheck):
   return len(tocheck) != len(set(tocheck))
+
+# return the average of the numbers in the list.
+def avg(lst):
+    return sum(lst)/len(lst) if len(lst) > 0 else 0.0
+
+# input: {i => j => v,...}
+# output: [(i,j,v),...]
+def kkv_to_tuples(kkv):
+    return [(i,j,v) for (i,jtov) in kkv.items() for j,v in jtov.items()]
+
+# input: [(i,j,v),...]
+# output: {i => j => v,...}
+def tuples_to_kkv(tuples):
+    i_s = set([i for (i,_,_) in tuples])
+    
+    kkv = {i:{} for i in i_s}
+    
+    for i,j,v in tuples:
+        kkv[i][j] = v
+        
+    return kkv
+
+# input: {i => j => v,...}
+# output: {j => i => v,...}
+def kkv_invert(kkv):
+    tuples = kkv_to_tuples(kkv)
+    
+    return tuples_to_kkv([(j,i,v) for (i,j,v) in tuples])   
