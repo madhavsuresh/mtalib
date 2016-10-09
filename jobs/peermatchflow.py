82db016fbd029b26e6228d99c3d5c582e77f4bc1
Jason Hartline <jasonhartline@gmail.com>
2016-10-09 15:44:07 -0500

    it is a big pain that many of the functions in this module use a global accessor.
    these edits created versions of some of the functions that do not use the global accessor.  the old version (with global accessor) was then modified to call the one with the explicit accessor.
    
    the 'random_ta_assignment' function was moved to algo/peer_assignment and renamed 'random_assignment'
    
    some functions were rewritten to be more pythonic.

--- jobs/peermatchflow.py
+++ jobs/peermatchflow.py
@@ -4,40 +4,61 @@ from .. import config
 
 c = config.api_server
 
+def execute_peermatch_from_accessor(accessor,assignmentID, cover = [], load=3):
+    
+    peer_and_submission_ids = accessor.peermatch_get_peer_and_submission_ids(assignmentID)['peerSubmissionPairs']
+    subs = [(d['peerID'],d['submissionID']) for d in peer_and_submission_ids]
+    exclude = {i:[j] for i,j in subs}
+
+    (submissions,peers) = zip(*subs)
+    peers = list(peers)
+    submissions = list(submissions)
+    
+    assignments = assignment.peer_assignment_covered(peers, submissions, load, cover=cover, excludes=exclude)
+    
+    if assignment.peer_assignment_check(peers,assignments,exclude):
+        return (assignments, cover)
+    
+    print 'error'
+    return None
+
 def execute_peermatch(assignmentID, cover=[], load=3):
+  return execute_peermatch_from_accessor(c,assignmentID,cover,load)
+
+  # this is old code.
   peer_and_submission_ids = c.peermatch_get_peer_and_submission_ids(assignmentID)['peerSubmissionPairs']
   exclude = dict((x,[y,]) for x,y in [tuple(d.values()) for d in peer_and_submission_ids])
   submissions = [d['submissionID'] for d in peer_and_submission_ids]
   peers = [d['peerID'] for d in peer_and_submission_ids]
   assignments = peer_assignment_covered(peers, submissions, load, cover=cover, excludes=exclude)
   if peer_assignment_check(peers,assignments,exclude):
-    return {'assignments': assignments, 'cover': cover}
+    return (assignments,cover)
   print 'error'
   return None
 
-def convert_alg_to_api_matching(matchings):
-  peer_matchings_api_fmt = []
-  for peerID, submissionIDs in matchings.iteritems():
-    for submissionID in submissionIDs:
-      peer_matchings_api_fmt.append({"reviewerID": peerID, "submissionID": submissionID})
-  return peer_matchings_api_fmt
+
+def matching_to_mechta_matching(matchings):
+  pairs = kvs_to_pairs(matchings)
+  return [{'reviewerID': i, 'submissionID': j} for i,j in pairs]
+
 
 def insert_peermatch(assignmentID):
-  assignments_and_cover = execute_peermatch(assignmentID)
-  assignments = assignments_and_cover['assignments']
-  cover = assignments_and_cover['cover']
-  peer_assignments_api_fmt = convert_alg_to_api_matching(assignments)
-  c.peermatch_create_bulk(assignmentID, peer_assignments_api_fmt)
+  (assignments,cover) = execute_peermatch(assignmentID)
+  mechta_matching = matching_to_mechta_matching(assignments)
+  c.peermatch_create_bulk(assignmentID, mechta_matching)
   return cover
 
+def insert_ta_matches_from_accessor(accessor,assignmentID, cover):
+  courseID = int(accessor.get_courseID_from_assignmentID(assignmentID)['courseID'])
+  taIDs = accessor.get_tas_from_course(courseID)['taIDs']
+  matching = random_assignment(taIDs, cover)
+  mechta_matching = matching_to_mechta_matching(matching)
+  return accessor.peermatch_create_bulk(assignmentID, mechta_matching)
+
+
 def insert_ta_matches(assignmentID, cover):
-  courseID = int(c.get_courseID_from_assignmentID(assignmentID)['courseID'])
-  taIDs = c.get_tas_from_course(courseID)['taIDs']
-  n = len(taIDs)
-  m = len(cover)
-  matching = random_ta_assignment(taIDs, cover)
-  matching_api_fmt = convert_alg_to_api_matching(matching)
-  c.peermatch_create_bulk(Assignmentid, matching_api_fmt)
+  return insert_ta_matches_from_accessor(c,assignmentID, cover)
+
 
 def execute(assignmentID):
     cover = insert_peermatch(assignmentID)
